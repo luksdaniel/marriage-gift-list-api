889f1c46e3fee09d8050137116384310ec131681
Lucas Daniel Batista <luksdanielbatista2@gmail.com>
2025-08-05 09:39:51 -0300

    Base Java-Spring application

---
+++ src/main/java/com/lucasbatista/projects/MarriageGiftListApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbatista.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarriageGiftListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarriageGiftListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasbatista/projects/MarriageGiftListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbatista.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarriageGiftListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
